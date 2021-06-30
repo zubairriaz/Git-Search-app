2c70d43b8a77c377837db4868793e83fad206874
Zubair <zubair.riaz@trapezegroup.com>
2021-07-01 03:23:31 +0500

    testing axios

--- scalio-test/src/__tests__/utils.js
+++ scalio-test/src/__tests__/utils.js
@@ -1,4 +1,15 @@
-import { extractData, invert, toggle, copy, sortObj } from "../utils/utils";
+import {
+	extractData,
+	invert,
+	toggle,
+	copy,
+	sortObj,
+	request,
+    
+} from "../utils/utils";
+import {url} from "../utils/constants"
+import mockAxios from "axios";
+import { expect } from "@jest/globals";
 
 describe("extract data", () => {
 	test("extraact Data Working Correctly", () => {
@@ -27,43 +38,53 @@ describe("Toggle function", () => {
 });
 
 describe("Test copy method", () => {
-    let obj;
-    beforeEach(()=>{
-        obj = {a:1,c:2,d:{e:4}}
-    })
+	let obj;
+	beforeEach(() => {
+		obj = { a: 1, c: 2, d: { e: 4 } };
+	});
 	test("Copy should deep copy the object", () => {
-        let obj2 = copy(obj);
-        obj2.d.e = 6;
+		let obj2 = copy(obj);
+		obj2.d.e = 6;
 		expect(obj).not.toEqual(obj2);
-        expect(obj.d.e).toBe(4)
+		expect(obj.d.e).toBe(4);
 	});
 
-    test("Passing falsy value should return undefined ", () => {
-        let obj2 = copy("");
-        expect(obj2).toBe(undefined)
+	test("Passing falsy value should return undefined ", () => {
+		let obj2 = copy("");
+		expect(obj2).toBe(undefined);
 	});
 });
-
 
 describe("Test sort Method", () => {
-    let obj,obj1,sortFunc
-    beforeEach(()=>{
-        obj = {name:"Apple"}
-        obj1= {name:"Zcom"}
-        sortFunc = sortObj("name")
-
-
-    })
+	let obj, obj1, sortFunc;
+	beforeEach(() => {
+		obj = { name: "Apple" };
+		obj1 = { name: "Zcom" };
+		sortFunc = sortObj("name");
+	});
 	test("It should provide -1 when first argument is alphbatically lesser than the latter", () => {
-        let result = sortFunc(obj, obj1);
-        expect(result).toBe(-1)
+		let result = sortFunc(obj, obj1);
+		expect(result).toBe(-1);
 	});
 
-    test("It should provide 1 when first argument is alphbatically greater than the latter", () => {
-        let result = sortFunc(obj1, obj);
-        expect(result).toBe(1)
+	test("It should provide 1 when first argument is alphbatically greater than the latter", () => {
+		let result = sortFunc(obj1, obj);
+		expect(result).toBe(1);
 	});
-
-  
 });
 
+describe("Test axios request method",  () => {
+	test("Request should call axios get method with url", async () => {
+        let dispatch = jest.fn();
+		mockAxios.get.mockImplementationOnce(() =>
+			Promise.resolve({
+				data: { results: ["dummyData"] },
+			})
+		);
+
+        const apiRequest = request(dispatch);
+         await apiRequest(url);
+        expect(mockAxios.get).toHaveBeenCalledTimes(1);
+        expect(mockAxios.get).toHaveBeenCalledWith(url)
+	});
+});
